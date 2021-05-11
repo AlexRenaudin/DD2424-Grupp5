faba7ae9c148bc67d8bc4e626762b7fee6c9c392
Alex <alren@kth.se>
2021-05-11 10:51:27 +0200

    structure

--- Project.py
+++ Project.py
@@ -1,3 +1,4 @@
+#################### Final Code ##################### 
 # Import
 import torch
 import torch.nn as nn
@@ -9,7 +10,7 @@ import torchvision.transforms as transforms
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Parameters
-n_epochs = 10
+n_epochs = 1
 n_batch = 100
 eta = 0.0001
 T = 25 #seq_length 
@@ -19,13 +20,13 @@ momentum = 0.99
 # Data
 
 #transform = ???, 
-
+'''
 trainset = torchvision.datasets.???(root='./data', train=True, transform=transform, download=True)
 trainloader = torch.utils.data.DataLoader(dataset=trainset, batch_size=n_batch, shuffle=True)
 
 testset = torchvision.datasets.???(root='./data', train=False, transform=transform, download=True)
 testloader = torch.utils.data.DataLoader(dataset=testset, batch_size=n_batch, shuffle=False)
-
+'''
 #classes = ???
 
 # Network
@@ -37,21 +38,27 @@ class Net(nn.Module):
     def forward(self, x):
         h0 = torch.zeros(1, 1, 100).to(device) 
         c0 = torch.zeros(1, 1, 100).to(device) 
-        x = self.lstm(x, (h0, c0))  
+        x = self.lstm(x, (h0, c0))
         return x
 
 # Training
+'''
 net = Net().to(device)
 optimizer = optim.SGD(net.parameters(), lr=eta, momentum = momentum)
 criterion = nn.CrossEntropyLoss()
 for epoch in range(n_epochs):
-    for i, data in enumerate(train_loader):
+    for i, data in enumerate(trainloader):
         inputs, labels = data.to(device)
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+'''
+#################################################### 
 
-# Generate Text
+# Debugging
+
+
+
 
