1119cfd22752aa9d1586b1e364b5dcbd00dfc2ba
Alex <alren@kth.se>
2021-05-15 14:57:20 +0200

    just backup, don't pull

--- Project.py
+++ Project.py
@@ -12,28 +12,13 @@ from torchtext.vocab import Vocab
 # GPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Vocabulary
-vocab_iter = PennTreebank(split='train')
-tokenizer = get_tokenizer('basic_english')
-counter = Counter()
-for line in vocab_iter:
-    #print(f'line = {tokenizer(line)}')
-    lines = tokenizer(line)
-    for l in lines: 
-        counter.update(list(l[:]))
-print(f'counter = {counter}')
-vocab = Vocab(counter)
-
-
-# Data
-train_iter, val_iter, test_iter = PennTreebank()
-def data_process(raw_text_iter, vocab):
-  data = [torch.tensor([vocab[token] for token in tokenizer(item)],
-                       dtype=torch.float) for item in raw_text_iter]
-  return torch.cat(tuple(filter(lambda t: t.numel() > 0, data)))
-train_data = data_process(train_iter, vocab)
-val_data = data_process(val_iter, vocab)
-test_data = data_process(test_iter, vocab)
+# Unique Characters
+unique_text = PennTreebank(split='train')
+unique = Counter()
+for line in unique_text:
+    for word in line:
+        for c in word:
+            unique.update(c)
 
 # Parameters
 n_epochs = 1
@@ -41,42 +26,72 @@ eta = 0.0001
 seq_len  = 25
 momentum = 0.99
 m = 100
-K = len(vocab)
+K = len(unique)
+
+# Dictionaries 
+int2char = {}
+char2int = {}
+k = 0
+for c in unique:
+    int2char[k] = c
+    char2int[c] = k
+    k += 1
+'''
+# One-Hot Encoding
+def one_hot_encode(seq, K):
+    one_hot = np.zeros((K, np.multiply(*seq.shape)), dtype=np.float32)
+    one_hot[-1-seq.flatten(), np.arange(one_hot.shape[1])] = 1.
+    return one_hot
+'''
+# Data
+try:
+    train_tensor = torch.load('train_tensor.pt')
+    val_tensor = torch.load('val_tensor.pt')
+    test_tensor = torch.load('test_tensor.pt')
+except FileNotFoundError:
+    train_text, val_text, test_text = PennTreebank()
+    def text2tensor(length, text, seq_len, K, char2int):
+        tensor = torch.zeros(seq_len, (length//seq_len)+1, K)
+        i = 0
+        for line in text:
+            for word in line:
+                for c in word:
+                    tensor[i%seq_len, i//seq_len, char2int[c]] = 1
+                    i += 1
+        return tensor
+    train_tensor = text2tensor(5101618, train_text, seq_len, K, char2int)
+    torch.save(train_tensor, 'train_tensor.pt')
+    val_tensor = text2tensor(399782, val_text, seq_len, K, char2int)
+    torch.save(val_tensor, 'val_tensor.pt')
+    test_tensor = text2tensor(449945, test_text, seq_len, K, char2int)
+    torch.save(test_tensor, 'test_tensor.pt')
 
 # Network
 class Net(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers):
+    def __init__(self, input_size, hidden_size):
         super(Net, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.rnn = nn.RNN(input_size = input_size, hidden_size = hidden_size)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        print("x.size(0): " + str(x.size()))
-        print("h0.size(): " + str(h0.size())) 
-        print("c0.size(): "+ str(c0.size())) 
-        out, _ = self.lstm(x, (h0,c0))
-        out = out[:, -1, :]
-        return out
+        h0 = torch.zeros(1, self.input_size, self.hidden_size).to(device)
+        xnext, _ = self.rnn(x, h0)
+        xnext = xnext[:, -1, :]
+        return xnext
 
 # Initialization 
-net = Net(seq_len, m, 1).to(device)
+net = Net(seq_len, m).to(device)
 optimizer = optim.SGD(net.parameters(), lr=eta, momentum = momentum)
 criterion = nn.CrossEntropyLoss()
-E = train_data.size(0) - seq_len
 
 # Training
 for epoch in range(n_epochs):
-    e = 0
-    while e <= E:
-        X = train_data[e:e+seq_len]
-        Y = train_data[e+1:e+seq_len+1]
+    for i in range(train_tensor.size(1)):
+        X = train_tensor[:,i,:]
+        Y = torch.cat((train_tensor[1:seq_len,i,:],train_tensor[seq_len-1:seq_len,i+1,:]),0)
         loss = criterion(net(X), Y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        e = e + seq_len
-        print(e)
+        print(i)
