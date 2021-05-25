682131b308f22c162b03c9f11a1b18e51076a654
Saman Rassam <16594517+samanrassam@users.noreply.github.com>
2021-05-25 15:28:15 +0200

    Added comments

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
 import matplotlib.pyplot as plt
 from Project import run_model
 import numpy as np
+import time
 
-#model_list = ['rnn', 'lstm', '2-lstm', 'bi-lstm', 'gru']
-model_list = ['rnn', 'lstm']
+# Run models in model_list and store training/test loss
+
+model_list = ['rnn', 'lstm', '2-lstm', 'bi-lstm', 'gru']
+#model_list = ['rnn', 'lstm']
+
+training_loss_list = []
 test_loss_list = []
-val_loss_list = []
 for model in model_list:
+    start_time = time.time()
     t_loss, v_loss = run_model(model)
-    test_loss_list.append(t_loss)
-    val_loss_list.append(v_loss)
+    training_loss_list.append(t_loss)
+    test_loss_list.append(v_loss)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
-for idx, test_loss in enumerate(test_loss_list):
-    plt.plot(test_loss, label= model_list[idx] + ' test loss')
-    savetxt(model_list[idx] +'_train.csv', test_loss, delimiter=',')
+
+
+#Plot and save training loss
+
+for idx, training_loss in enumerate(training_loss_list):
+    plt.plot(training_loss, label= model_list[idx] + ' Training loss')
+    np.savetxt(model_list[idx] +'_train_with_conv.csv', training_loss, delimiter=',')
     
-plt.title('Training losses of various models on Penn Treebank')
+plt.title('Training losses of various models on Goblet Book dataset')
 plt.xlabel('Sequence')
 plt.ylabel('Loss')
 plt.legend()
 plt.figure(1)
 
 plt.figure(2)
-for idx, val_loss in enumerate(val_loss_list):
-    plt.plot(val_loss, label= model_list[idx] + ' val loss')
-    savetxt(model_list[idx] +'_test.csv', test_loss, delimiter=',')
-plt.title('Test losses of various models on Penn Treebank')
+
+# Plot and save validation loss
+
+for idx, test_loss in enumerate(test_loss_list):
+    plt.plot(test_loss, label= model_list[idx] + ' Test loss')
+    np.savetxt(model_list[idx] +'_test_with_conv.csv', test_loss, delimiter=',')
+plt.title('Test losses of various models on Golbet Book dataset')
 plt.xlabel('Sequence')
 plt.ylabel('Loss')
 plt.legend()
 
 
 plt.show()
+
+
+
 
 
